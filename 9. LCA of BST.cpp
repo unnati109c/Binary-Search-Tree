51d1ec0dd425d2665c16c457c70c7fef6f08b471
unnati109c <67646140+unnati109c@users.noreply.github.com>
2021-07-23 17:25:32 +0530

    Medium

---
+++ 9. LCA of BST.cpp
@@ -0,0 +1,30 @@
+Node* LCA(Node *root, int n1, int n2)
+{
+//   if(!root) return NULL;
+//   while(root!=NULL)
+//   {
+//       if(root->data > n1 and root->data > n2)
+//       {
+//           root = root->left;
+//       }
+//       else if(root->data < n1 and root->data < n2)
+//       {
+//           root = root->right;
+//       }
+//       else
+//       {
+//           return root;
+//       }
+//   }
+//   return root;
+
+     if (root == NULL) return NULL;
+
+     if (root->data > n1 && root->data > n2)
+     return LCA(root->left, n1, n2);
+
+     if (root->data < n1 && root->data < n2)
+     return LCA(root->right, n1, n2);
+
+     return root;
+}
